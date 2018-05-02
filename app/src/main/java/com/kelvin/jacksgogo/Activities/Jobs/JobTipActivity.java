57d14b67e357c8b310f4bc8294078dd3bc987cc2
goldenprovider0415 <topdeveloper1212@gmail.com>
2018-05-03 02:48:52 +0800

    [new] Reviewe, Tip UI

---
+++ app/src/main/java/com/kelvin/jacksgogo/Activities/Jobs/JobTipActivity.java
@@ -0,0 +1,15 @@
+package com.kelvin.jacksgogo.Activities.Jobs;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.kelvin.jacksgogo.R;
+
+
+public class JobTipActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jobtip);
+    }
+}
